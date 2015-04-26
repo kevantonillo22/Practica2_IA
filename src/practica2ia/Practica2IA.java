222e78fad240ef6ec49b10db80ef010b9f2d5473
Kev <car.kevin.22@gmail.com>
2015-04-26 12:33:51 -0600

    Creacion de proyecto
    Se creo el frame principal sin ninguna funcionalidad.

---
+++ src/practica2ia/Practica2IA.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica2ia;
+
+/**
+ *
+ * @author Kev
+ */
+public class Practica2IA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        mainFrame frame = new mainFrame();
+        frame.setVisible(true);
+    }
+    
+}
